d2941bdaae14ef8199249b06cda16f5a95683ff1
arohnid <dimaalexxxx@gmail.com>
2025-07-01 18:22:31 +0300

    Project init

---
+++ src/main/java/com/example/book_account/BookAccount.java
@@ -0,0 +1,11 @@
+package com.example.book_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAccount {
+    public static void main(String[] args) {
+        SpringApplication.run(BookAccount.class, args);
+    }
+}
