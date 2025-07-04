f8d9aae79fe45dab1799c5f07f0e3bb98be0c3b6
arohnid <dimaalexxxx@gmail.com>
2025-07-04 03:03:50 +0300

    Deleted data-test.sql

--- src/test/resources/data-test.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO author(name, birth_year) VALUES('Test Author 1', 1991);
-INSERT INTO author(name, birth_year) VALUES('Test Author 2', 1964);
-INSERT INTO author(name, birth_year) VALUES('Test Author 3', 1871);
